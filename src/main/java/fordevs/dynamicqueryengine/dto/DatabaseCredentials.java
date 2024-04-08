391d82ff0568364c0c26d3a94e2906d4dd74742d
Enoc Velásquez <fungicoder@gmail.com>
2024-04-07 22:24:50 -0600

    Add user database credentials DTO class

---
+++ src/main/java/fordevs/dynamicqueryengine/dto/DatabaseCredentials.java
@@ -0,0 +1,17 @@
+package fordevs.dynamicqueryengine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DatabaseCredentials {
+    private String databaseManager;
+    private String host;
+    private Integer port;
+    private String databaseName;
+    private String user;
+    private String password;
+}
