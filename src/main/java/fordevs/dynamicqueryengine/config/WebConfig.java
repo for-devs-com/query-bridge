8afc215c2c046a4c61d1ab0ebbd9c0c2648c61d6
Enoc Velásquez <fungicoder@gmail.com>
2024-04-17 15:57:33 -0600

    Configured CORS settings in WebConfig for Spring Boot application

---
+++ src/main/java/fordevs/dynamicqueryengine/config/WebConfig.java
@@ -0,0 +1,18 @@
+package fordevs.dynamicqueryengine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // Configuración de la aplicación
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
+
